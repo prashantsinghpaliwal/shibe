6fc59e2c421a9cbcbc5a4d038816b84e614f4ebb
BigStep <bbdgoc@gmail.com>
2021-06-22 00:44:15 +0530

    Custom image library code

--- app/src/main/java/com/prashant/shibe/MainActivity.kt
+++ app/src/main/java/com/prashant/shibe/MainActivity.kt
@@ -3,6 +3,8 @@ package com.prashant.shibe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.prashant.imageloader.CustomImageLoader
 import com.prashant.imageloader.ImageLoader
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         imageView = findViewById(R.id.image)
 
-        ImageLoader.displayImage(imageView,"https://cdn.shibe.online/shibes/feedcc9b2735b82f936fae19ed88ed89d6562cd7.jpg")
+        ImageLoader.with(this)
+            .placeHolder(ContextCompat.getDrawable(this,R.drawable.ic_photo_24)!!)
+            .load(imageView,
+            "https://cdn.shibe.online/shibes/e056e6b3d0d17628d1771f8559b6cb629dc5f455.jpg")
     }
 }
--- imageloader/src/main/java/com/prashant/imageloader/ImageLoader.kt
+++ imageloader/src/main/java/com/prashant/imageloader/ImageLoader.kt
@@ -1,60 +1,135 @@
 package com.prashant.imageloader
 
-
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.Handler
-import android.os.Looper
+import android.graphics.drawable.Drawable
 import android.util.LruCache
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.net.HttpURLConnection
 import java.net.URL
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.util.*
+import java.util.Collections.synchronizedMap
 
-object ImageLoader {
+class ImageLoader(context: Context) {
 
+    private val maxCacheSize: Int = (Runtime.getRuntime().maxMemory() / 1024).toInt() / 8
+    private val memoryCache: LruCache<String, Bitmap>
 
-    private var imageCache: LruCache<String, Bitmap>
-    private var executorService: ExecutorService =
-        Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
-    private val uiHandler: Handler = Handler(Looper.getMainLooper())
+    private val imageViewMap = synchronizedMap(WeakHashMap<ImageView, String>())
+    private var placeHolder: Drawable? = null
 
     init {
-        val maxMemory: Long = Runtime.getRuntime().maxMemory() / 1024
-        val cacheSize: Int = (maxMemory / 4).toInt()
-
-        imageCache = object : LruCache<String, Bitmap>(cacheSize) {
-            override fun sizeOf(key: String?, bitmap: Bitmap?): Int {
-                return (bitmap?.rowBytes ?: 0) * (bitmap?.height ?: 0) / 1024
+        memoryCache = object : LruCache<String, Bitmap>(maxCacheSize) {
+            override fun sizeOf(key: String, bitmap: Bitmap): Int {
+                // The cache size will be measured in kilobytes rather than number of items.
+                return bitmap.byteCount / 1024
             }
+        }
+
+        val metrics = context.resources.displayMetrics
+        screenWidth = metrics.widthPixels
+        screenHeight = metrics.heightPixels
+    }
+
+    companion object {
+
+        private var INSTANCE: ImageLoader? = null
+
+        internal var screenWidth = 0
+        internal var screenHeight = 0
+
+        @Synchronized
+        fun with(context: Context): ImageLoader {
+
+            require(context != null) {
+                "ImageLoader:with - Context should not be null."
+            }
+
+            return INSTANCE ?: ImageLoader(context).also {
+                INSTANCE = it
+            }
+
         }
     }
 
-    fun displayImage(imageView: ImageView, url: String) {
-        val cached = imageCache.get(url)
-        if (cached != null) {
-            updateImageView(imageView, cached)
-            return
+    fun load(imageView: ImageView, imageUrl: String) {
+
+        require(imageView != null) {
+            "ImageLoader:load - ImageView should not be null."
         }
 
-        imageView.tag = url
-        executorService.submit {
-            val bitmap: Bitmap? = downloadImage(url)
-            if (bitmap != null) {
-                if (imageView.tag == url) {
-                    updateImageView(imageView, bitmap)
+        require(imageUrl != null && imageUrl.isNotEmpty()) {
+            "ImageLoader:load - Image Url should not be empty"
+        }
+
+        imageView.setImageResource(0)
+        imageViewMap[imageView] = imageUrl
+
+        placeHolder?.let {
+            imageView.setImageDrawable(placeHolder)
+        }
+
+        val bitmap = checkImageInCache(imageUrl)
+        bitmap?.let {
+            loadImageIntoImageView(imageView, it, imageUrl)
+        } ?: run {
+            imageView.tag = imageUrl
+            CoroutineScope(Dispatchers.IO).launch {
+                val bitmap = downloadImage(imageUrl)
+                withContext(Dispatchers.Main) {
+                    if (bitmap != null) {
+                        if (imageView.tag == imageUrl) {
+                            updateImageView(imageView, bitmap, imageUrl)
+                        }
+                        memoryCache.put(imageUrl, bitmap)
+                    }
                 }
-                imageCache.put(url, bitmap)
             }
+        }
+
+    }
+
+    private fun updateImageView(imageView: ImageView, bitmap: Bitmap, imageUrl: String) {
+        val scaledBitmap = Utils.scaleBitmapForLoad(bitmap, imageView.width, imageView.height)
+        scaledBitmap?.let {
+            if (!isImageViewReused(ImageRequest(imageUrl, imageView))) {
+                imageView.post {
+                    imageView.setImageBitmap(scaledBitmap)
+                }
+            }
+        }
+
+    }
+
+    private fun isImageViewReused(imageRequest: ImageRequest): Boolean {
+        val tag = imageViewMap[imageRequest.imageView]
+        return tag == null || tag != imageRequest.imgUrl
+    }
+
+    @Synchronized
+    private fun checkImageInCache(imageUrl: String): Bitmap? = memoryCache.get(imageUrl)
+
+    @Synchronized
+    private fun loadImageIntoImageView(imageView: ImageView, bitmap: Bitmap?, imageUrl: String) {
+
+        require(bitmap != null) {
+            "ImageLoader:loadImageIntoImageView - Bitmap should not be null"
+        }
+
+        val scaledBitmap = Utils.scaleBitmapForLoad(bitmap, imageView.width, imageView.height)
+        scaledBitmap?.let {
+            if (!isImageViewReused(ImageRequest(imageUrl, imageView)))
+                imageView.setImageBitmap(scaledBitmap)
         }
     }
 
-    private fun updateImageView(imageView: ImageView, bitmap: Bitmap) {
-        uiHandler.post {
-            imageView.setImageBitmap(bitmap)
-        }
-    }
+    inner class ImageRequest(var imgUrl: String, var imageView: ImageView)
+
 
     private fun downloadImage(url: String): Bitmap? {
         var bitmap: Bitmap? = null
@@ -68,5 +143,10 @@ object ImageLoader {
         }
 
         return bitmap
+    }
+
+    fun placeHolder(placeHolder: Drawable): ImageLoader {
+        this.placeHolder = placeHolder
+        return INSTANCE!!
     }
 }
